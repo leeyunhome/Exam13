c3956e8c54c11a605c44c1544aaecaae84df3f9a
leeyunhome <leeyunhome@daum.net>
2020-03-07 13:03:54 +0900

    Initial commit

---
+++ app/src/main/java/com/egloos/neuezeal/exam13/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.egloos.neuezeal.exam13
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        moveButton.setOnClickListener(object:View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(applicationContext, SubActivity::class.java)
+                intent.putExtra("MESSAGE", "MainActivity에서 왔습니다")
+                startActivity(intent)
+            }
+
+        })
+    }
+}
---
+++ app/src/main/java/com/egloos/neuezeal/exam13/SubActivity.kt
@@ -0,0 +1,23 @@
+package com.egloos.neuezeal.exam13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_sub.*
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+        val intent = getIntent()
+
+        extraText.text = intent.getStringExtra("MESSAGE")
+
+        okButton.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                finish()
+            }
+        })
+    }
+}
